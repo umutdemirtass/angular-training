346e9e5c67bf9bc05d9d5b4f0f31b70ec9d41d3f
Umut Demirtaş <82568079+umutdemirtass@users.noreply.github.com>
2021-05-14 18:09:18 +0300

    comment name change

--- src/app/services/folders.service.ts
+++ src/app/services/folders.service.ts
@@ -9,7 +9,7 @@ export class FoldersService {
   path2 = 'https://count-book-on-premises-inventory-service-dev.azurewebsites.net/api/v1/Folder/GetFoldersWithItems/';
 
   constructor(private http: HttpClient) { }
-  // Orta Klasorler
+  // Second Page
   getFoldersById(id: any): Observable<any> {
     return this.http.get(this.path2 + id + '?page=0&limit=10');
   }
